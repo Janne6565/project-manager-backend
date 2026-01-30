f0e046d562a56e1b96697f17d8b79ffc1d43532e
Janne Keipert <jabbekeipert@gmail.com>
2026-01-30 14:15:12 +0100

    Add Spring Security configuration with basic authentication and update dependencies

---
+++ src/main/java/com/janne6565/projectmanager/configs/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.janne6565.projectmanager.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .anyRequest().hasRole("ADMIN")
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
