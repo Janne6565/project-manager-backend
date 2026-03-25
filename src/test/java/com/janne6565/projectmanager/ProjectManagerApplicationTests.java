ba2f4a016d7e3782b65e9b6737c8d4b5bdb46622
Janne Keipert <jabbekeipert@gmail.com>
2026-03-25 12:20:44 +0100

    Mock `ExternalContributionService` in application tests and configure default response

--- src/test/java/com/janne6565/projectmanager/ProjectManagerApplicationTests.java
+++ src/test/java/com/janne6565/projectmanager/ProjectManagerApplicationTests.java
@@ -1,12 +1,29 @@
 package com.janne6565.projectmanager;
 
+import com.janne6565.projectmanager.dto.external.contributions.ContributionSummaryDto;
+import com.janne6565.projectmanager.dto.external.contributions.ContributionTotalsDto;
+import com.janne6565.projectmanager.services.external.ExternalContributionService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ProjectManagerApplicationTests {
+
+    @MockitoBean
+    private ExternalContributionService externalContributionService;
+
+    @BeforeEach
+    void setup() {
+        ContributionSummaryDto emptySummary = new ContributionSummaryDto(
+                java.util.Map.of(), java.util.List.of(), new ContributionTotalsDto(0, 0, 0, 0));
+        org.mockito.Mockito.when(externalContributionService.getContributions())
+                .thenReturn(Mono.just(emptySummary));
+    }
 
     @Test
     void contextLoads() {
